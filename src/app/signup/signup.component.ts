27a9d44bfc5582b516b0db57eb5decd083a6d0b1
Lucas Mouraud <mouraud.lucas@gmail.com>
2019-02-04 11:59:57 +0100

    affichage du mail quand le champ est rempli

--- src/app/signup/signup.component.ts
+++ src/app/signup/signup.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import {  Email } from './email'
 
 @Component({
   selector: 'app-signup',
@@ -7,7 +8,10 @@ import { Component, OnInit } from '@angular/core';
 })
 export class SignupComponent implements OnInit {
 
-  mail = 'mouraud.lucas@gmail.com';
+  mail: Email = {
+    id: 1,
+    value: 'example.mail@gmail.com'
+  };
   constructor() { }
 
   ngOnInit() {
